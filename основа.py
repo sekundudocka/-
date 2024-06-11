0366be7fd6ecb474063755c92a0f6fd291154d4d
sekundudocka <kirsumail@gmail.com>
2024-06-11 15:05:41 +0300

    Add files via upload

--- основа.py
+++ основа.py
@@ -1,4 +1,4 @@
-import pygame, sys , random
+import pygame, sys , random , time
 import pygame as pg
 
 pg.init()
@@ -31,50 +31,70 @@ class Wall:
     def draw(self):
         screen.blit(self.img, (self.rect.x, self.rect.y))
 
-#рандомайзер поля
-pole = [[0,0,0,0],
-       [0,0,0,0],    
-       [0,0,0,0],
-       [0,0,0,0],
-        ]
-
-walls = []
-
-
-for line in range(len(pole)):
-    for cell in range(len(pole[line])):
-            walls.append(Wall(cell * 150, line * 150, 150, 150, 'поле.jpg', cell, line))
-
-rows = len(pole)
-cols = len(pole[0])
-
-for i in range(3):
-    for j in range(3):
-        pole[i][j] = random.randint(0, 1) 
-
 def check(pole , i , j):
     countOfbomb = 0
     for x in range(max(0, i-1), min(rows,i+2)):
         for y in range(max(0, j-1), min(cols,j+2)):
-            if pole[x][y] == 1:
-                countOfbomb += 2 
+            if pole[x][y] == '*':
+                countOfbomb += 1
 
-    return countOfbomb 
+    return countOfbomb
 
-for i in range(rows):
-    for j in range(cols):
-        if pole[i][j] != 1:
-            pole[i][j] = check(pole , i ,  j)
+def generatePole(rows , cols , mines):
+    pole = []   
+    for i in range(rows):
+        pole.append([])
+        for j in range(cols):
+            pole[i].append(0) 
 
 
-file = open('map3.txt' , 'w')
-file.write('')
-file.close()
-file = open('map3.txt' , 'a')
-for i in pole:
-    for j in i:
-        file.write(str(j))
-    file.write('\n')
+    minecount = 0
+    while minecount < mines:
+        i = random.randint(0 , rows - 1)               
+        j = random.randint(0 , cols - 1)   
+        if pole[i][j] != '*':
+            pole[i][j] = "*"
+            minecount += 1
+
+    return pole                
+
+
+
+
+#рандомайзер поля
+pole = generatePole(4 , 4 , 6)
+pole1 = generatePole(8 , 8 , 28)
+pole2 = generatePole(16 , 16 , 84)
+
+walls = []
+walls2 = []
+walls3 = []
+
+for line in range(len(pole)):
+    for cell in range(len(pole[line])):
+            walls.append(Wall(cell * 150, line * 150, 150, 150, 'пусто.png', cell, line))
+
+for line in range(len(pole1)):
+    for cell in range(len(pole1[line])):
+            walls2.append(Wall(cell * 75, line * 75, 75, 75, 'пусто.png', cell, line))
+
+for line in range(len(pole2)):
+    for cell in range(len(pole2[line])):
+            walls3.append(Wall(cell * 37, line * 37, 37, 37, 'пусто.png', cell, line))      
+
+rows = len(pole)
+cols = len(pole[0])
+
+rows1 = len(pole1)
+cols1 = len(pole1[0])
+
+rows2 = len(pole2)
+cols2 = len(pole2[0])
+
+# for i in range(rows):
+#     for j in range(cols):
+#         if pole[i][j] != 1:
+#             pole[i][j] = check(pole , i ,  j)
 
 print(pole[0])
 print(pole[1])
@@ -84,10 +104,12 @@ print(pole[3])
 #весь текст
 font = pygame.font.Font('mine-sweeper.ttf', 50)
 font2 = pygame.font.Font('mine-sweeper.ttf', 45)
+font3 = pygame.font.Font('mine-sweeper.ttf', 40)
 message1 = font.render('''start''', True ,(0, 0, 0) )
 x4 = font2.render('''4 x 4''', True ,(0, 0, 0) )
-x8 = font2.render('''4 x 4''', True ,(0, 0, 0) )
-x16 = font2.render('''4 x 4''', True ,(0, 0, 0) )
+x8 = font2.render('''8 x 8''', True ,(0, 0, 0) )
+x16 = font3.render('''16 x 16''', True ,(0, 0, 0) )
+win = font2.render('''you win!!!''', True ,(0, 0, 0) )
 
 #кнопки
 button = pygame.Rect(150, 150, 300 , 100)
@@ -103,6 +125,8 @@ hardColor3 = (50 , 50 , 50)
 
 fon = Any(0 , 0 , 600 , 6000 , 'поле.jpg')
 pos1 = Any(0 , 0 , 100 , 100 , 'bomb.png')
+
+found = 0
 
 #надо
 screen = pygame.display.set_mode((X , Y))
@@ -146,6 +170,10 @@ while True:
                 x , y = event.pos
                 if 200 < x < 400 and 150 < y < 250:
                     game_state = 3
+                elif 200 < x < 400 and 300 < y < 400:
+                    game_state = 4
+                elif 200 < x < 400 and 450 < y < 550:
+                    game_state = 5  
 
         pygame.draw.rect(screen, hardColor1 , hard1)        
         pygame.draw.rect(screen, hardColor2 , hard2)         
@@ -154,6 +182,16 @@ while True:
         screen.blit(x8,(205 , 320)) 
         screen.blit(x16,(205 , 470)) 
 
+    if game_state == 'lose':
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        for Wall in walls:
+            Wall.draw()       
+        time.sleep(3)
+        sys.exit()
+        
     if game_state == 3:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -162,24 +200,92 @@ while True:
                 for wall in walls:
                     if wall.rect.collidepoint(event.pos[0], event.pos[1]):
                         mine = (pole[wall.row][wall.col])
-                        if mine == 1:
-                            sys.exit()
+
+                        if mine == '*':
+                            wall.img = pygame.image.load('bomb.png')  
+                            wall.img = pygame.transform.scale(wall.img, (wall.rect.width, wall.rect.height))                                                      
+                            sound1.play()
+
+                            game_state = 'lose'
+
                         else:
-                            print('норм')
+                            sound2.play()
+                            boms = check(pole , wall.row , wall.col)
+                            wall.img = pygame.image.load(str(boms) + '.png')
+                            wall.img = pygame.transform.scale(wall.img, (wall.rect.width, wall.rect.height))
+                            found += 1
+                            if found == 10:                             
+                                game_state = 'win'
+
+        for Wall in walls:
+            Wall.draw()
+
+    if game_state == 4:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for wall in walls2:
+                    if wall.rect.collidepoint(event.pos[0], event.pos[1]):
+                        mine = (pole1[wall.row][wall.col])
+
+                        if mine == '*':
+                            wall.img = pygame.image.load('bomb.png')  
+                            wall.img = pygame.transform.scale(wall.img, (wall.rect.width, wall.rect.height))                                                      
+                            sound1.play()
+
+                            game_state = 'lose'
+
+                        else:
+                            sound2.play()
+                            boms = check(pole1 , wall.row , wall.col)
+                            wall.img = pygame.image.load(str(boms) + '.png')
+                            wall.img = pygame.transform.scale(wall.img, (wall.rect.width, wall.rect.height))
+                            found += 1
+                            if found == 36:                              
+                                game_state = 'win'
+
+        for Wall in walls2:
+            Wall.draw()
+
+    if game_state == 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for wall in walls3:
+                    if wall.rect.collidepoint(event.pos[0], event.pos[1]):
+                        mine = (pole2[wall.row][wall.col])
+                        if mine == '*':
+                            wall.img = pygame.image.load('bomb.png')  
+                            wall.img = pygame.transform.scale(wall.img, (wall.rect.width, wall.rect.height))                                                      
+                            sound1.play()
+
+                            game_state = 'lose'
+
+                        else:
+                            sound2.play()
+                            boms = check(pole2 , wall.row , wall.col)
+                            wall.img = pygame.image.load(str(boms) + '.png')
+                            wall.img = pygame.transform.scale(wall.img, (wall.rect.width, wall.rect.height))
+                            found += 1
+                            if found == 174:
+                                game_state = 'win'
+
+        for Wall in walls3:
+            Wall.draw()
+
+    if game_state == 'win':
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.blit(win,(130 , 250))
+
+
+
 
     pygame.display.update()
     clock.tick(60)
 
     
   
-        # for event in pygame.event.get():
-        #     if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_SPACE:
-        #             game_state = 1
-
-        #     if event.type == pygame.QUIT:
-        #         sys.exit()
-
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #   if click.collidepoint(event.pos[0], event.pos[1]):
-        #     event.pos - это координаты нажатия [0] - x [1] - y
